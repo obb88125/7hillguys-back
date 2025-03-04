583b9de70036b29a5ecf7f0a828bb48d9d6e17b7
uchong <dmlcksghd@gmail.com>
2025-03-04 16:15:04 +0900

    Type: Feat
    Body: 투자 심자 데이터 API
    - 투자 데이터 DB 저장
    
    Footer:
     Resolves:
     Related to:

---
+++ src/main/java/com/shinhan/peoch/invest/controller/UserProfileController.java
@@ -0,0 +1,24 @@
+package com.shinhan.peoch.invest.controller;
+
+import com.shinhan.entity.UserProfileEntity;
+import com.shinhan.peoch.invest.dto.UserProfileDTO;
+import com.shinhan.peoch.invest.service.UserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profile")
+@RequiredArgsConstructor
+public class UserProfileController {
+    private final UserProfileService userProfileService;
+
+    @PostMapping("save")
+    public ResponseEntity<UserProfileEntity> saveUserProfile(@RequestBody UserProfileDTO dto) {
+        UserProfileEntity savedProfile = userProfileService.saveUserProfile(dto);
+        return ResponseEntity.ok(savedProfile);
+    }
+}
