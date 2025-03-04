de60b90fca1a0d853daf30929e75da59c439c427
uchong <dmlcksghd@gmail.com>
2025-03-04 15:14:58 +0900

    Type: Feat
    Body: 로그인, 로그아웃의 JWT 수정
    - 로그아웃시 JWT 블랙리스트 저장
    
    Footer:
     R제esolves:
     Related to:

--- src/main/java/com/shinhan/peoch/auth/entity/UserEntity.java
+++ src/main/java/com/shinhan/peoch/auth/entity/UserEntity.java
@@ -1,49 +1,50 @@
-//package com.shinhan.peoch.auth.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.UpdateTimestamp;
-//
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//
-//@Getter @Setter
-//@ToString
-//@Builder
-//@NoArgsConstructor @AllArgsConstructor
-//
-//@Entity
-//@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
-//public class UserEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long userId;
-//
-//    @Column(nullable = false, length = 255, unique = true)
-//    private String email;
-//
-//    @Column(nullable = false, length = 255)
-//    private String password;
-//
-//    @Column(nullable = false, length = 100)
-//    private String name;
-//
-//    private LocalDate birthdate;
-//
-//    private String phone;
-//
-//    private String address;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false, length = 10)
-//    private UserRole role;
-//
-//    @CreationTimestamp
-//    private LocalDateTime createdAt;
-//
-//    @UpdateTimestamp
-//    private LocalDateTime updatedAt;
-//
-//}
+package com.shinhan.peoch.auth.entity;
+
+import com.shinhan.entity.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    private LocalDate birthdate;
+
+    private String phone;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private UserRole role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
