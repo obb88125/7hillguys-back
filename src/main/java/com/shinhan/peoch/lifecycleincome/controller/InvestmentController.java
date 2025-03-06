18828f2f59d7d6fc56befff0eb8867c667e636df
obb88125 <obb88125@gmail.com>
2025-03-06 14:48:35 +0900

    Type: Feat
    Body:
    투자 금액에 따른 납부비율 선정하는 서비스 및 컨트롤러
    
    Footer:
    
    Related to: #7

--- src/main/java/com/shinhan/peoch/lifecycleincome/controller/InvestmentController.java
+++ src/main/java/com/shinhan/peoch/lifecycleincome/controller/InvestmentController.java
@@ -4,10 +4,8 @@ package com.shinhan.peoch.lifecycleincome.controller;
 import com.shinhan.entity.InvestmentEntity;
 import com.shinhan.peoch.lifecycleincome.service.InvestmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +17,10 @@ public class InvestmentController {
     @GetMapping("/investment/{userId}")
     public InvestmentEntity saveInvestment(@PathVariable Integer userId) {
         return investmentService.createInvestment(userId);
+    }
+    @GetMapping("/investment/{userId}/refund-rate")
+    public ResponseEntity<Double> updateRefundRate(@PathVariable Integer userId) {
+        double refundRate = investmentService.updateRefundRate(userId);
+        return ResponseEntity.ok(refundRate);
     }
 }
