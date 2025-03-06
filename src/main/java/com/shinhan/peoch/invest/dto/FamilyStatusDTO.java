3099f5c71466bb3f27808441690cf44aca955ab1
uchong <dmlcksghd@gmail.com>
2025-03-06 15:08:37 +0900

    Type: Feat
    Body: 투자 심사 API
    - 투자 심사 API에서 json 값으로 받는 DTO 생성
    
    Footer:
     Resolves:
     Related t정o:

---
+++ src/main/java/com/shinhan/peoch/invest/dto/FamilyStatusDTO.java
@@ -0,0 +1,13 @@
+package com.shinhan.peoch.invest.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FamilyStatusDTO {
+    private boolean married;
+    private String children;
+}
---
+++ src/main/java/com/shinhan/peoch/invest/dto/studentCardDTO.java
@@ -0,0 +1,13 @@
+package com.shinhan.peoch.invest.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class studentCardDTO {
+    private String highscool;
+    private String highscoolGPA;
+}
