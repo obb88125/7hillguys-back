55ad1d72287796faf37bc4816821a9ed78420dfc
uchong <dmlcksghd@gmail.com>
2025-03-10 14:26:35 +0900

    Type: Feat
    Body: 투자 심사 결과
    - DB 컬럼명 변경 후 InvestmentService 적용되는지 확인
    
    Footer:
     Resolves:
     Related to:
    김

--- src/main/java/com/shinhan/peoch/invest/service/InvestmentService.java
+++ src/main/java/com/shinhan/peoch/invest/service/InvestmentService.java
@@ -1,33 +1,34 @@
-//package com.shinhan.peoch.invest.service;
-//
-//import com.shinhan.entity.InvestmentEntity;
-//import com.shinhan.repository.InvestmentRepository;
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class InvestmentService {
-//    private final InvestmentRepository investmentRepository;
-//
-//    @Transactional(ReadOnly=true)
-//    public String getInvestmentStatus(Integer grantId) {
-//        Optional<InvestmentEntity> investment = investmentRepository.findById(grantId);
-//
-//        if(investment.isPresent()) {
-//            switch(investment.get().getStatus()) {
-//                case 승인:
-//                    return "approved";
-//                case 거절:
-//                    return "rejected";
-//                case 대기:
-//                    return "pending";
-//            }
-//        } else {
-//            throw new IllegalArgumentException("투자 정보를 찾을 수 없습니다.");
-//        }
-//    }
-//}
+package com.shinhan.peoch.invest.service;
+
+import com.shinhan.entity.InvestmentEntity;
+import com.shinhan.repository.InvestmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class InvestmentService {
+    private final InvestmentRepository investmentRepository;
+
+    @Transactional(readOnly = true)
+    public String getInvestmentStatus(Integer grantId) {
+        Optional<InvestmentEntity> investment = investmentRepository.findById(grantId);
+
+        if (investment.isPresent()) {
+            switch (investment.get().getStatus()) {
+                case 승인:
+                    return "approved"; // 승인된 경우
+                case 거절:
+                    return "rejected"; // 거절된 경우
+                case 대기:
+                default:
+                    return "pending"; // 대기 중인 경우
+            }
+        } else {
+            throw new IllegalArgumentException("투자 정보를 찾을 수 없습니다.");
+        }
+    }
+}
