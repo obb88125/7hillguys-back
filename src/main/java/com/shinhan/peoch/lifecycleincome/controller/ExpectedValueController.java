b1f80cc63c0aad566a771ffbb8ee3aca91aa9526
obb88125 <obb88125@gmail.com>
2025-03-11 15:24:13 +0900

    Type: Fix
    Body:
    Controller 통합
    
    Footer:
    
    Related to: #25

--- src/main/java/com/shinhan/peoch/lifecycleincome/controller/ExpectedValueController.java
+++ src/main/java/com/shinhan/peoch/lifecycleincome/controller/ExpectedValueController.java
@@ -1,12 +1,17 @@
 package com.shinhan.peoch.lifecycleincome.controller;
 
 
+import com.shinhan.entity.ExpectedIncomeEntity;
+import com.shinhan.peoch.lifecycleincome.service.ExpectedIncomeService;
 import com.shinhan.peoch.lifecycleincome.service.ExpectedValueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,9 +19,18 @@ public class ExpectedValueController {
 
     @Autowired
     private ExpectedValueService expectedValueService;
+    @Autowired
+    private ExpectedIncomeService expectedIncomeService;
 
     @GetMapping("/expectedvalue/{userId}")
     public Double getExpectedValue(@PathVariable Integer userId) {
         return expectedValueService.calculatePresentValue(userId);
+    }
+    @GetMapping("/expectedincome/{userProfileId}")
+    public ResponseEntity<List<ExpectedIncomeEntity>> getExpectedIncomes(
+            @PathVariable Integer userProfileId) {
+
+        List<ExpectedIncomeEntity> incomes = expectedIncomeService.getExpectedIncomesByUserProfileId(userProfileId);
+        return ResponseEntity.ok(incomes);
     }
 }
