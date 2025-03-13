a74386892ed94f3e7202375aa33e3905e4bef7b3
obb88125 <obb88125@gmail.com>
2025-03-13 12:18:16 +0900

    Type: Fix Body: profile이 있는데 expectedIncome이 미정일 경우 예외처리
    Footer:
    
    Related to: #25

--- src/main/java/com/shinhan/repository/ExpectedIncomeRepository.java
+++ src/main/java/com/shinhan/repository/ExpectedIncomeRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExpectedIncomeRepository extends JpaRepository<ExpectedIncomeEntity, Integer> {
     List<ExpectedIncomeEntity> findByUserProfileId(Integer userProfileId);
     // 최신 updatedAt 기준으로 가장 최근 1개 엔티티 조회
-    ExpectedIncomeEntity findFirstByUserProfileIdOrderByCreatedAtDesc(Integer userProfileId);
+    Optional<ExpectedIncomeEntity> findFirstByUserProfileIdOrderByCreatedAtDesc(Integer userProfileId);
      // CreatedAt 기준으로 가장 과거 1개 엔티티 조회
-    ExpectedIncomeEntity findFirstByUserProfileIdOrderByCreatedAtAsc(Integer userProfileId);
+     Optional<ExpectedIncomeEntity> findFirstByUserProfileIdOrderByCreatedAtAsc(Integer userProfileId);
 }
