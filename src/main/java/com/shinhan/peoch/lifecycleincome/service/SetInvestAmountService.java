a044c39f91275bb048ccee1a64da9167693cf1bd
obb88125 <obb88125@gmail.com>
2025-03-13 15:07:47 +0900

    Type: Feat
    Body:
    Optional에 따른
    에러 처리메세지 및 레포 autowired
    
    Footer:
    
    Related to: #25

--- src/main/java/com/shinhan/peoch/lifecycleincome/service/SetInvestAmountService.java
+++ src/main/java/com/shinhan/peoch/lifecycleincome/service/SetInvestAmountService.java
@@ -3,11 +3,15 @@ package com.shinhan.peoch.lifecycleincome.service;
 import com.shinhan.entity.ExpectedIncomeEntity;
 import com.shinhan.entity.InflationRateEntity;
 import com.shinhan.entity.InvestmentEntity;
+import com.shinhan.entity.UserProfileEntity;
 import com.shinhan.peoch.lifecycleincome.DTO.SetInvestAmountDTO;
 import com.shinhan.repository.ExpectedIncomeRepository;
 import com.shinhan.repository.InflationRateRepository;
 import com.shinhan.repository.InvestmentRepository;
+import com.shinhan.repository.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -15,15 +19,23 @@ import java.time.LocalDate;
 @Service
 @RequiredArgsConstructor
 public class SetInvestAmountService {
+    @Autowired
+    InvestmentRepository investmentRepository;
+    @Autowired
+    ExpectedIncomeRepository expectedIncomeRepository;
+    @Autowired
+    InflationRateRepository inflationRateRepository;
 
-    private final InvestmentRepository investmentRepository;
-    private final ExpectedIncomeRepository expectedIncomeRepository;
-    private final InflationRateRepository inflationRateRepository;
+    @Autowired
+    UserProfileRepository userProfileRepository;
 
     public SetInvestAmountDTO getInvestmentData(Integer userProfileId) {
+        UserProfileEntity userProfileEntity = userProfileRepository.findByUserProfileId(userProfileId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저 프로필 정보를 찾을 수 없습니다."));
         // 1. 투자 정보 조회
-        InvestmentEntity investment = investmentRepository.findFirstByUserIdOrderByUpdatedAtDesc(userProfileId);
-
+        InvestmentEntity investment = investmentRepository.findFirstByUserIdOrderByUpdatedAtDesc(userProfileEntity.getUserId())
+                .orElseThrow(() -> new UsernameNotFoundException("해당 투자 정보를 찾을 수 없습니다."));
+        System.out.println(investment);
         // 2. 예상 소득 데이터 조회
         ExpectedIncomeEntity incomes = expectedIncomeRepository.findFirstByUserProfileIdOrderByCreatedAtDesc(userProfileId).
         orElseThrow(() -> new RuntimeException("사용자 예상 소득 정보를 찾을 수 없습니다."));
