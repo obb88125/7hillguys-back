e8f335241ce66dd5ab0104fc3a42b569ddd0db52
obb88125 <obb88125@gmail.com>
2025-03-13 12:18:57 +0900

    Type: Feat
    Body:
    income 없을 경우 대비 예외 에러 처리
    
    Footer:
    
    Related to: #25

--- src/main/java/com/shinhan/peoch/lifecycleincome/service/SetInvestAmountService.java
+++ src/main/java/com/shinhan/peoch/lifecycleincome/service/SetInvestAmountService.java
@@ -10,6 +10,8 @@ import com.shinhan.repository.InvestmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 @RequiredArgsConstructor
 public class SetInvestAmountService {
@@ -23,11 +25,12 @@ public class SetInvestAmountService {
         InvestmentEntity investment = investmentRepository.findFirstByUserIdOrderByUpdatedAtDesc(userProfileId);
 
         // 2. 예상 소득 데이터 조회
-        ExpectedIncomeEntity incomes = expectedIncomeRepository.findFirstByUserProfileIdOrderByCreatedAtDesc(userProfileId);
+        ExpectedIncomeEntity incomes = expectedIncomeRepository.findFirstByUserProfileIdOrderByCreatedAtDesc(userProfileId).
+        orElseThrow(() -> new RuntimeException("사용자 예상 소득 정보를 찾을 수 없습니다."));
 
 
         // 3. 물가상승률 데이터 조회 (2025년 고정)
-        InflationRateEntity inflationRateEntity = inflationRateRepository.findByYear(2025);
+        InflationRateEntity inflationRateEntity = inflationRateRepository.findByYear(LocalDate.now().getYear());
 
 
         return SetInvestAmountDTO.builder()
