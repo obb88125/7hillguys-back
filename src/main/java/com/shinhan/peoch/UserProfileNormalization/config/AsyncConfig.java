9e4c313011723289bea80ea230d30b06563acb7b
obb88125 <obb88125@gmail.com>
2025-03-19 19:13:01 +0900

    Type: Feat
    Body:비동기 처리를 위한
    구현

---
+++ src/main/java/com/shinhan/peoch/UserProfileNormalization/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.shinhan.peoch.UserProfileNormalization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;  // 추가된 import
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+}
