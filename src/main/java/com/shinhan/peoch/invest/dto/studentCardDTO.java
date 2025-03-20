f35e75d062e473551a41ca96765b796616d5438e
uchong <dmlcksghd@gmail.com>
2025-03-20 16:21:52 +0900

    Type: Fix
    Body: studentCarDTO 오타 수정
    
    Footer:
     Resolve제s:
     Related to:

--- src/main/java/com/shinhan/peoch/invest/dto/studentCardDTO.java
+++ src/main/java/com/shinhan/peoch/invest/dto/studentCardDTO.java
@@ -8,6 +8,6 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class studentCardDTO {
-    private String highscool;
-    private String highscoolGPA;
+    private String highschool;
+    private String highschoolGPA;
 }
